fd2459d71f429c046ea3cb7738e0623589c79f6a
Can Gündoğdu <gundogdu.can@hotmail.com>
2021-01-21 00:03:39 +0300

    summary

---
+++ OOP1/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product 
+    {
+        //snipped=Hazır kodlar
+        //product=Ürün
+
+
+        public int Id  { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string ProductName  { get; set; }
+
+        //unit price=Ürün birim fiyatı
+        public double UnitPrice { get; set; }
+
+        //units in stock=Ürün stok adedi
+        public int UnitsInStock { get; set; }
+
+
+    }
+}
---
+++ Ref-Out_Ödev_Örneği/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ref_Out_Ödev_Örneği
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2=80;
+            var result = Topla(ref number1,number2);
+            var result2 = Topla2(out number1, number2);
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine(number1);
+            Console.ReadLine();
+
+        }
+
+        static int Topla2(out int number1, int number2)
+        {
+
+
+           number1 = 50;
+            return number1 + number2;
+        }
+
+        static int Topla(ref int number1,int number2)
+        {
+
+
+            number1 = 20;
+            return number1 + number2;
+        }
+
+
+    }
+
+}  
