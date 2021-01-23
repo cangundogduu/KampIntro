f6cfc4bf56556d527706c19041663e0c57a51881
Can Gündoğdu <gundogdu.can@hotmail.com>
2021-01-23 23:59:04 +0300

    Revert "Revert "Summary""
    This reverts commit 29928e77faded9932c105507ae18782b36713a91.

---
+++ OOP2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Engin Demiroğ-Gerçek Müşteri
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "12345678910";
+
+            //Kodlama.io-Tüzel Müşteri
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+            
+            
+            //new= aslında referans numarası demektir.
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+
+
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ıhtıyacKrediManager = new IhtıyacKrediManager();           
+            IKrediManager tasitKrediManager = new TasitKrediManager();            
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager,databaseLoggerService);
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ıhtıyacKrediManager,tasitKrediManager };
+           // basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+            Console.ReadLine();
+        }
+    }
+}
