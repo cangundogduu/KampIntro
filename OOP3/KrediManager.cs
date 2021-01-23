a3e05add76d5cc81dd6c963c21d1b184e6a8f18f
Can Gündoğdu <gundogdu.can@hotmail.com>
2021-01-23 23:55:41 +0300

    Summary

---
+++ OOP3/KrediManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    interface IKrediManager
+        //eğer birisi bu interfaceyi kullanırsa buranın içindeki fonksiyonu kullanmak zorundadır.
+        //Interface= Şablon
+        //interfaceleri birbirinin alternatifi ama kod içerikleri farklı olan methotlar-sınıf için kullanılır.
+    {
+        void Hesapla();
+        void BirseyYap();
+    }
+}
